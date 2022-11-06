206bf854196fe5393dd5a932cd119c24c1c8c886
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-11-06 22:49:58 +0700

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
 
 export const API = axios.create({
-	baseURL: 'http://localhost:8000/api/v1',
+	baseURL: 'https://waysfood-rest.herokuapp.com/api/v1',
 });
 
 export const setAuthToken = (token) => {
 	if (token != '') {
 		API.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-		
 	} else {
 		delete API.defaults.headers.common['Authorization'];
 	}
